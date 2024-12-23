49d6337d6a89b6e0d71453f1e68702d5ea62c301
Yasser Hajlaoui <yasser159@gmail.com>
2024-12-22 22:13:58 -0800

    Initial Commit

---
+++ NavigationStackTutorial/NavigationStackTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackTutorialApp.swift
+//  NavigationStackTutorial
+//
+//  Created by Yasser Hajlaoui on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
